4e732c88b666eb09825c52af7717cc16942483e7
Kirill GPRB <41549137+undnull@users.noreply.github.com>
2022-11-30 00:51:03 +0500

    Upload the static HTML map

---
+++ custom.markers.js
@@ -0,0 +1,16 @@
+UnminedCustomMarkers = {
+    isEnabled: true,
+    markers: [
+        // Players
+        { x: -359,  z: 267, image: "custom.player.png",     imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+        { x: -7378, z: 860, image: "custom.player.png",     imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+        { x: -87,   z: 248, image: "custom.player.png",     imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+        { x: -295,  z: 74,  image: "custom.player.png",     imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+        { x: -7379, z: 863, image: "custom.player.png",     imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+        { x: -7469, z: 797, image: "custom.player.png",     imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+
+        // Worldspawn
+        // NOTE: this shall remain the last marker in the list
+        { x: -132,  z: 256, image: "custom.worldspawn.png", imageAnchor: [0.5, 1.0], imageScale: 0.25 },
+    ]
+}
